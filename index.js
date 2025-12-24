4e90319997c52081a50eb62e4f45f04c521e445c
la-877 <lunaadam41@gmail.com>
2025-12-24 20:22:39 +1100

    checked how to access data

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 
-const loc = 'london';
+const loc = document.getElementById('location');
+const subButtn = document.getElementById('submit');
+const todaysTemp = document.getElementById('current-temperature');
 
-function fetchApi() {
+function fetchApi(location) {
+    const date = new Date();
+    const currentDate = date.toISOString().slice(0, 10);
     const myKey = '9NG57RXYMHCK8BH8HTVRY8P2L'
-    const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?key=${myKey}`;
-    
+    const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${currentDate}?key=${myKey}`;
+
     fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok: ' + response.statusText);
-                
+
             }
             return response.json();
         })
 
         .then(data => {
-            console.log(data);
+            todaysTemp.textContent = data.days[0].temp;
+            console.log();
         })
 
         .catch(error => {
             console.error('Error:', error);
         });
-}
+};
 
-fetchApi();
+subButtn.addEventListener('click', () => {
+    const inputLoc = loc.value;
+    if (inputLoc) {
+        console.log();
+        fetchApi(inputLoc);
+    }
+});
+
+
+
