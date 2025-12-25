4e57127b6a6cc6ffcfef160738263c81dadd2b09
la-877 <lunaadam41@gmail.com>
2025-12-26 00:03:01 +1100

    the api can already get ranges between the day before, the current day and next day based on the timezone so changed the url and added the timezones current hour

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ const usrLoc = document.getElementById('user-location');
 
 
 function fetchApi(location) {
-    
-    const date = new Date();
-    const currentDate = date.toISOString().slice(0, 10);
+
     const myKey = '9NG57RXYMHCK8BH8HTVRY8P2L';
-    const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${currentDate}?key=${myKey}`;
+    const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/yesterday/tomorrow/?key=${myKey}`;
+
 
     fetch(apiUrl)
         .then(response => {
@@ -29,13 +28,23 @@ function fetchApi(location) {
         .then(data => {
             todaysTemp.textContent = data.days[0].temp;
             usrLoc.textContent = data.timezone;
-            //console.log(data.days[0]);
-            const currentTime = date.getHours();
+            console.log(data.days);
+
+            
+            const date = new Date();
+            const localTime = date.toLocaleTimeString('en-US', {
+                timeZone: data.timezone,
+                hour: 'numeric',
+                hour12: false,
+            });
+            
+            const currentTime = localTime;
+
 
             windSpeed.textContent = data.days[0].windspeed;
             rain.textContent = data.days[0].precip;
             data.days[0].hours.forEach(hr => {
-                
+
                 renderHourInfo(hr.datetime.slice(0, 2), hr.icon, hr.temp, currentTime)
 
             });
@@ -48,54 +57,53 @@ function fetchApi(location) {
         });
 };
 
+navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude.toFixed(4);
+    const lon = position.coords.longitude.toFixed(4);
+    const coords = `${lat},${lon}`;
 
+    fetchApi(coords);
+});
 
 subButtn.addEventListener('click', () => {
     const inputLoc = loc.value;
     if (inputLoc) {
-        hrsContainer.innerHTML= '';
+        hrsContainer.innerHTML = '';
         fetchApi(inputLoc);
     }
-    else {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const lat = position.coords.latitude.toFixed(4);
-            const lon = position.coords.longitude.toFixed(4);
-            const coords = `${lat},${lon}`;
-            
-            fetchApi(coords); 
-        })}
-    
+
+
 });
 
-function renderHourInfo(hrTime, cndition, hrTemp, currHour){
+function renderHourInfo(hrTime, cndition, hrTemp, currHour) {
     const hrContainer = document.createElement('div');
     hrContainer.className = 'hr-container';
-    
+
     const hour = document.createElement('p');
     hour.className = 'hour';
-    if(currHour === parseInt(hrTime)){
+    if (currHour === parseInt(hrTime)) {
         hrContainer.id = 'current-time';
-        
+
     }
     if (hrTime.startsWith('0')) {
-        hrTime = hrTime.substring(1); 
-        
+        hrTime = hrTime.substring(1);
+
     }
-    if (parseInt(hrTime) < 12){
-        if(parseInt(hrTime) === 0){
+    if (parseInt(hrTime) < 12) {
+        if (parseInt(hrTime) === 0) {
             hrTime = '12';
         }
         hour.textContent = hrTime + 'am';
-        
+
     }
     else {
         hrTime = parseInt(hrTime) - 12;
-        if(hrTime === 0){
+        if (hrTime === 0) {
             hrTime = 12;
         }
         hour.textContent = hrTime + 'pm';
     }
-    
+
     const condition = document.createElement('p');
     condition.className = 'condition-icon';
     condition.textContent = cndition;
